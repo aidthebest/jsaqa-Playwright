e43972ae322de2aee98f27bc0133302e31cca0f6
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-11-06 20:34:09 +0300

    start work

--- tests/NetologyTest.spec.js
+++ tests/NetologyTest.spec.js
@@ -1,28 +1,32 @@
 const { chromium } = require("playwright");
 
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 (async () => {
   const browser = await chromium.launch({
     headless: false,
     slowMo: 5000,
     devtools: true,
   });
-  const page = browser.newPage();
+  const page = await browser.newPage();
 
   (await page).goto("https://netology.ru");
 
   (await page).click("text=Войти");
 
-  // (await page).click('[type="password"]');
+  await sleep(4000);
 
   (await page).fill('[type="password"]', "pass");
 
-  (await page).pause();
-
-  // (await page).click('[placeholder="Email"]');
+  await sleep(4000);
 
   (await page).fill('[placeholder="Email"]', "mail@mail.ru");
 
-  (await page).pause();
+  (await page).pause(); // включает режим инспектора
 
   (await page).click('button:has-text("ОК")');
 
