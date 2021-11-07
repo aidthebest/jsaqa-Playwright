dd7b8fb07cbc8b5b708c0ba11d5d0bf0cc24f770
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-11-07 19:09:34 +0300

    strange works...

--- tests/NetologyTest.spec.js
+++ tests/NetologyTest.spec.js
@@ -1,4 +1,7 @@
 const { chromium } = require("playwright");
+const { mail, pass } = require("../user");
+
+const { test, expect } = require("@playwright/test");
 
 function sleep(ms) {
   return new Promise((resolve) => {
@@ -6,35 +9,67 @@ function sleep(ms) {
   });
 }
 
-const { mail, pass } = require("../user");
+(async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 5000,
+    // devtools: true,
+  });
+  const page = await browser.newPage();
+
+  await page.goto("https://netology.ru");
+
+  await page.click("text=Войти");
+
+  // await sleep(4000); //если все данные вносятся в одно поле раскомментить
+
+  await page.fill('[type="password"]', pass);
+
+  // await sleep(4000); //если все данные вносятся в одно поле раскомментить
+
+  await page.fill('[placeholder="Email"]', mail);
+
+  // await page.pause(); // включает режим инспектора
+
+  await page.click('button:has-text("Войти")');
+
+  // await sleep(4000);
+
+  await expect(page.locator("text=Мои курсы и профессии")).toBeVisible();
+
+  await browser.close();
+})();
 
 (async () => {
   const browser = await chromium.launch({
     headless: false,
     slowMo: 5000,
-    devtools: true,
+    // devtools: true,
   });
-  const page = browser.newPage();
 
-  (await page).goto("https://netology.ru");
+  const page = await browser.newPage();
 
-  (await page).click("text=Войти");
+  await page.goto("https://netology.ru");
+
+  await page.click("text=Войти");
+
+  await sleep(4000); //если все данные вносятся в одно поле раскомментить
+
+  (await page).fill('[type="password"]', "pass");
 
   await sleep(4000);
 
-  // (await page).fill('[type="password"]', "pass");
+  (await page).fill('[placeholder="Email"]', "mail@mail.ru");
 
-  (await page).fill('[type="password"]', pass);
+  // await page.pause(); // включает режим инспектора
 
-  await sleep(4000);
+  await page.click('button:has-text("Войти")');
 
-  // (await page).fill('[placeholder="Email"]', "mail@mail.ru");
+  // await sleep(4000); //если все данные вносятся в одно поле раскомментить
 
-  (await page).fill('[placeholder="Email"]', mail);
+  await expect(
+    page.locator("text=Вы ввели неправильно логин или пароль")
+  ).toBeVisible();
 
-  (await page).pause(); // включает режим инспектора
-
-  (await page).click('button:has-text("ОК")');
-
-  (await page).click('button:has-text("Войти")');
+  await browser.close();
 })();
